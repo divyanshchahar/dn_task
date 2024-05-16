073a90dc5685a28fffac1f880b3d850fa70414ff
divyanshchahar <divyanshchahar@outlook.com>
2024-05-16 23:18:05 +0530

    added db connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,19 @@
+const mongoose = require('mongoose');
 
+// establishing connection to database using promise based approach
+const createConnection = async () => {
+	const connection = await mongoose
+		.createConnection(process.env.DB)
+		.asPromise();
+
+	if (connection.readyState) console.log('Established connection to database');
+
+	try {
+	} catch (error) {
+		console.error(
+			`The following erreo occurred while connectiong to DB: ${error}`
+		);
+	}
+};
+
+createConnection();
